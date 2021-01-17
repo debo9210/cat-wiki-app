f76833fc5b948e78a3291371a919ab7db1aba773
debo9210 <omololuadebowale1@gmail.com>
2021-01-17 01:36:18 -0500

    make app ready for production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const catWiki = require('./routes/api/catWiki');
 
@@ -21,11 +22,21 @@ mongoose
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to cat wiki');
-});
+// app.get('/', (req, res) => {
+//   res.send('Welcome to cat wiki');
+// });
 
 app.use('/api/cat-wiki', catWiki);
+
+//server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = 5000 || process.env.PORT;
 
